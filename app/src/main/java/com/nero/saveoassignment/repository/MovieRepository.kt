9c695b14aabdff672272ca47b07f72b43655b22e
Dheeraj Gupta <dhiraj02gupta@gmail.com>
2021-07-25 15:54:12 +0530

    mvvm + pagging

--- app/src/main/java/com/nero/saveoassignment/repository/MovieRepository.kt
+++ app/src/main/java/com/nero/saveoassignment/repository/MovieRepository.kt
@@ -3,12 +3,23 @@ package com.nero.saveoassignment.repository
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.liveData
+import com.nero.moviereview.utils.Resource
+import com.nero.saveoassignment.data.model.MovieResponse
 import com.nero.saveoassignment.data.remote.MovieClient
+import com.nero.saveoassignment.utils.Constants.START_PAGE
 import com.nero.saveoassignment.utils.MoviePagingSource
 import javax.inject.Inject
 
 class MovieRepository @Inject constructor(private val movieClient: MovieClient) {
 
+    suspend fun getMovies(): Resource<MovieResponse> {
+        val response = try {
+            movieClient.movieList(START_PAGE)
+        } catch (e: Exception) {
+            return Resource.Error("${e.localizedMessage}")
+        }
+        return Resource.Success(response)
+    }
 
     fun getSearchResults() =
         Pager(
