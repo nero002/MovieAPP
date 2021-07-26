de10ccc872b04522f5490984118c2cfa5d5d2d91
Dheeraj Gupta <dhiraj02gupta@gmail.com>
2021-07-26 06:34:37 +0530

    done

--- app/src/main/java/com/nero/saveoassignment/utils/Constants.kt
+++ app/src/main/java/com/nero/saveoassignment/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.nero.saveoassignment.utils
 
 object Constants {
 
-    const val BASE_URL = " https://api.tvmaze.com/"
+    const val BASE_URL = "https://api.tvmaze.com/"
 
 
     const val START_PAGE = 1
--- app/src/main/java/com/nero/saveoassignment/views/SplashActivity.kt
+++ app/src/main/java/com/nero/saveoassignment/views/SplashActivity.kt
@@ -1,12 +1,20 @@
 package com.nero.saveoassignment.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.nero.saveoassignment.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MovieActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+        }, 1000)
     }
 }
